d6cb0a10cf89079b23b6ec59b4c4235510d08873
KalluruS <>
2021-04-18 07:48:20 +0530

    ss

--- vars/demo.groovy
+++ vars/demo.groovy
@@ -8,12 +8,12 @@ def call() {
         stage('Even Stage') {
           steps {
             echo "The build number is even"
-            // script { 
-            //   GroovyShell shell = new GroovyShell()
-            //   def prints = shell.parse(new File('./common/prints.groovy'))
-            //   prints.info 'Starting'
-            //   prints.warning 'Nothing to do!'
-            // }
+            script { 
+              // GroovyShell shell = new GroovyShell()
+              // def prints = shell.parse(new File('./common/prints.groovy'))
+              // prints.info 'Starting'
+              thing.warning 'Nothing to do!'
+            }
           }
         }
       }
